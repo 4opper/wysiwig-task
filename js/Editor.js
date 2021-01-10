94d3700bde9ca2f74e9fe1d7ee867522a8d3ac17
Roman Dukhno <4opper1@gmail.com>
2021-01-10 20:06:39 +0200

    fix selection, fix turning off styles

--- js/Editor.js
+++ js/Editor.js
@@ -6,6 +6,7 @@
 // TODO can try to get rid of filtered and simple selectedNodes to edit selection on selectionChanges
 // (filtered selectedNodes fixes case when end of prev line or start of next line is kinda selected, but no chars are selected on this prev/next line)
 // TODO mb perform normalize when unstyling
+// TODO fix ctrl + A doesnt style last node
 export class Editor {
   static selectRange = ({
     startNode,
@@ -24,12 +25,12 @@ export class Editor {
 
   getSelectedNodes = () => {
     var selection = window.getSelection();
-    var range = selection.getRangeAt(0)
-    if (selection.isCollapsed) {
+
+    if (selection.isCollapsed || !selection.rangeCount) {
       return [];
     }
 
-
+    var range = selection.getRangeAt(0)
     var node1 = selection.anchorNode;
     var node2 = selection.focusNode;
     var selectionAncestor = range.commonAncestorContainer;
@@ -94,7 +95,7 @@ export class Editor {
     }
   }
 
-  getSelectedTextNode = (node) => {
+  getTextNodes = (node) => {
     function recursor(n) {
       var i, a = [];
       if (n.nodeType !== 3) {
@@ -106,6 +107,12 @@ export class Editor {
       return a.filter(textNode => document.getSelection().containsNode(textNode));
     }
     return recursor(node);
+  }
+
+  getSelectedTextNodes = (node) => {
+    const allTextNodes = this.getTextNodes(node)
+
+    return allTextNodes.filter(textNode => document.getSelection().containsNode(textNode))
   }
   
   constructor (caret, editorNode) {
@@ -148,13 +155,19 @@ export class Editor {
 
   handleActionClick = (tagName) => {
     const { selectedNodes, filteredSelectedNodes, range } = this.getSelectedNodes()
+    
+    if (!range) {
+      console.log("not focused")
+      return 
+    }
+    
     const { startContainer, startOffset, endContainer, endOffset, collapsed } = range
     const updatedSelectedNodes = []
 
     // console.log("filteredSelectedNodes: ", filteredSelectedNodes)
 
     const isSelectionAlreadyWrapped = filteredSelectedNodes.every((selectedNode) => {
-      const textNodes = this.getSelectedTextNode(selectedNode)
+      const textNodes = this.getSelectedTextNodes(selectedNode)
       // console.log("textNodes: ", textNodes)
 
       const isAlreadyWrapped = textNodes.every(textNode => {
@@ -167,29 +180,93 @@ export class Editor {
       // console.log("isAlreadyWrapped: ", isAlreadyWrapped)
       return isAlreadyWrapped
     })
-    
+
     if (isSelectionAlreadyWrapped) {
       console.log("already wrapped")
       filteredSelectedNodes.forEach((selectedNode) => {
-        const textNodes = this.getSelectedTextNode(selectedNode)
+        const textNodes = this.getSelectedTextNodes(selectedNode)
+        // console.log("textNodes: ", textNodes)
+        // debugger
 
         textNodes.forEach((textNode, index) => {
-          const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-          const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
-          const styleNodeChild = styleNode.childNodes[0]
-          styleNode.replaceWith(styleNodeChild)
-          updatedSelectedNodes.push(styleNodeChild)
+          if (startContainer === endContainer) {
+            console.log("single text node")
+            const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+            const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+            const replaceWithNodes = []
+            const selectedText = range.toString()
+            const textBeforeSelected = textNode.data.slice(0, range.startOffset)
+            const textAfterSelected = textNode.data.slice(range.endOffset, textNode.length)
+
+            if (textBeforeSelected) {
+              const tagNode = document.createElement(tagName)
+              tagNode.append(textBeforeSelected)
+              replaceWithNodes.push(tagNode)
+            }
+
+            if (selectedText) {
+              const textNodeWithoutStyle = document.createTextNode(selectedText)
+              replaceWithNodes.push(textNodeWithoutStyle)
+              updatedSelectedNodes.push(textNodeWithoutStyle)
+            }
+
+            if (textAfterSelected) {
+              const tagNode = document.createElement(tagName)
+              tagNode.append(textAfterSelected)
+              replaceWithNodes.push(tagNode)
+            }
+
+            styleNode.replaceWith(...replaceWithNodes)
+          } else {
+            const isFirstLine = selectedNode === filteredSelectedNodes[0]
+            const isLastLine = selectedNode === filteredSelectedNodes[filteredSelectedNodes.length - 1]
+
+            if ((isFirstLine && startOffset === 0) || (isLastLine && endOffset === endContainer.length) || (!isFirstLine && !isLastLine))  {
+              console.log("one of multiple lines is fully selected")
+              const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+              const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+              styleNode.replaceWith(styleNode.firstChild)
+              updatedSelectedNodes.push(textNode)
+            } else {
+              if (isFirstLine) {
+                const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+                const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+                const notSelectedText = textNode.data.slice(0, startOffset)
+                const selectedText = textNode.data.slice(startOffset, textNode.length)
+                const textNodeWithoutStyle = document.createTextNode(selectedText)
+                const tagNode = document.createElement(tagName)
+                tagNode.append(notSelectedText)
+
+                styleNode.replaceWith(tagNode, textNodeWithoutStyle)
+                updatedSelectedNodes.push(textNodeWithoutStyle)
+              }
+
+              if (isLastLine) {
+                const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+                const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+                const selectedText = textNode.data.slice(0, endOffset)
+                const notSelectedText = textNode.data.slice(endOffset, textNode.length)
+                const textNodeWithoutStyle = document.createTextNode(selectedText)
+                const tagNode = document.createElement(tagName)
+                tagNode.append(notSelectedText)
+
+                styleNode.replaceWith(textNodeWithoutStyle, tagNode)
+                updatedSelectedNodes.push(textNodeWithoutStyle)
+              }
+            }
 
 
-          if (index === textNodes.length - 1) {
-            var a = styleNodeChild.parentNode
           }
+
+          // if (index === textNodes.length - 1) {
+          //   var a = styleNodeChild.parentNode
+          // }
 
         })
       })
     } else {
       filteredSelectedNodes.forEach((selectedNode, index) => {
-        const textNodes = this.getSelectedTextNode(selectedNode)
+        const textNodes = this.getSelectedTextNodes(selectedNode)
         const isAlreadyWrapped = textNodes.every(textNode => {
           const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
 
@@ -198,62 +275,63 @@ export class Editor {
 
         if (isAlreadyWrapped) {
           console.log("whole node is already wrapped")
-          updatedSelectedNodes.push(selectedNode)
+          const textNodes = this.getTextNodes(selectedNode)
+          updatedSelectedNodes.push(...textNodes)
           return
         }
 
         textNodes.forEach(textNode => {
           const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
           const isTextNodeAlreadyWrapped = textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
-          console.log("isTextNodeAlreadyWrapped: ", isTextNodeAlreadyWrapped)
 
           if (!isTextNodeAlreadyWrapped) {
-            if (selectedNodes.length === 1) /*Single selectedNode selected or has caret*/ {
+            if (startContainer === endContainer) /*Single selectedNode selected or has caret*/ {
               console.log("single selectedNode")
-              let nodeToSelect = textNode
-              let startOffsetToUse = startOffset
-              let endOffsetToUse = endOffset
+              // let nodeToSelect = textNode
+              // let startOffsetToUse = startOffset
+              // let endOffsetToUse = endOffset
 
-              if (collapsed) /*Nothing is selected*/ {
-                console.log("nothing is selected")
-                const charBeforeCaret = textNode.data[startOffset - 1]
-                const charAfterCaret = textNode.data[startOffset]
-
-                if (charBeforeCaret && charBeforeCaret !== ' ' && charAfterCaret &&  charAfterCaret !== ' ') /*Inside word*/ {
-                  const { range: wordRange } = this.caret.selectWordAtCaret()
-                  const word = wordRange.toString()
-                  const start = textNode.data.slice(0, wordRange.startOffset)
-                  const end = textNode.data.slice(wordRange.endOffset, textNode.length)
-                  const italicNode = document.createElement(tagName)
-                  if (tagName === 'h1' || tagName === 'h2') {
-                    italicNode.style.display = 'inline'
-                  }
-
-                  italicNode.append(word)
-
-                  textNode.replaceWith(start, italicNode, end)
-
-                  updatedSelectedNodes.push(italicNode)
-
-                  nodeToSelect = italicNode.firstChild
-                  startOffsetToUse = startOffset - start.length
-                  endOffsetToUse = startOffset - start.length
-                }
-
-                if (updatedSelectedNodes.length) {
-                  // this.setIsItalicIconActive(!this.isItalicActive)
-
-                  Editor.selectRange({
-                    startNode: nodeToSelect,
-                    startOffset: startOffsetToUse,
-                    endNode: nodeToSelect,
-                    endOffset: endOffsetToUse,
-                  })
-
-                }
-
-                return
-              } else /*Any part of selectedNode is selected*/  {
+              // if (collapsed) /*Nothing is selected*/ {
+              //   console.log("nothing is selected")
+              //   const charBeforeCaret = textNode.data[startOffset - 1]
+              //   const charAfterCaret = textNode.data[startOffset]
+              //
+              //   if (charBeforeCaret && charBeforeCaret !== ' ' && charAfterCaret &&  charAfterCaret !== ' ') /*Inside word*/ {
+              //     const { range: wordRange } = this.caret.selectWordAtCaret()
+              //     const word = wordRange.toString()
+              //     const start = textNode.data.slice(0, wordRange.startOffset)
+              //     const end = textNode.data.slice(wordRange.endOffset, textNode.length)
+              //     const italicNode = document.createElement(tagName)
+              //     if (tagName === 'h1' || tagName === 'h2') {
+              //       italicNode.style.display = 'inline'
+              //     }
+              //
+              //     italicNode.append(word)
+              //
+              //     textNode.replaceWith(start, italicNode, end)
+              //
+              //     updatedSelectedNodes.push(italicNode.firstChild)
+              //
+              //     nodeToSelect = italicNode.firstChild
+              //     startOffsetToUse = startOffset - start.length
+              //     endOffsetToUse = startOffset - start.length
+              //   }
+              //
+              //   if (updatedSelectedNodes.length) {
+              //     // this.setIsItalicIconActive(!this.isItalicActive)
+              //
+              //     Editor.selectRange({
+              //       startNode: nodeToSelect,
+              //       startOffset: startOffsetToUse,
+              //       endNode: nodeToSelect,
+              //       endOffset: endOffsetToUse,
+              //     })
+              //
+              //   }
+              //
+              //   return
+              // }
+              if (!collapsed) /*Any part of selectedNode is selected*/  {
                 console.log("inner part of word or whole word")
                 const replaceWithNodes = []
                 const selectedText = range.toString()
@@ -271,7 +349,7 @@ export class Editor {
 
                 italicNode.append(selectedText)
                 textNode.replaceWith(...replaceWithNodes)
-                updatedSelectedNodes.push(italicNode)
+                updatedSelectedNodes.push(italicNode.firstChild)
               }
             } else /*Multiple selectedNode selected or has caret*/ {
               const isFirstLine = selectedNode === filteredSelectedNodes[0]
@@ -285,9 +363,9 @@ export class Editor {
                 }
                 const clonedNode = textNode.cloneNode()
 
-                italicNode.appendChild(clonedNode)
+                italicNode.append(clonedNode)
                 textNode.replaceWith(italicNode)
-                updatedSelectedNodes.push(italicNode)
+                updatedSelectedNodes.push(italicNode.firstChild)
               } else {
                 if (isFirstLine) {
                   console.log("first of multiple lines is not fully selected")
@@ -295,12 +373,12 @@ export class Editor {
                   if (tagName === 'h1' || tagName === 'h2') {
                     italicNode.style.display = 'inline'
                   }
-                  const notSelectedPart = textNode.data.slice(0, startOffset)
-                  const selectedPart = textNode.data.slice(startOffset, textNode.length)
+                  const notSelectedText = textNode.data.slice(0, startOffset)
+                  const selectedText = textNode.data.slice(startOffset, textNode.length)
 
-                  italicNode.append(selectedPart)
-                  textNode.replaceWith(notSelectedPart, italicNode)
-                  updatedSelectedNodes.push(italicNode)
+                  italicNode.append(selectedText)
+                  textNode.replaceWith(notSelectedText, italicNode)
+                  updatedSelectedNodes.push(italicNode.firstChild)
                 }
 
                 if (isLastLine) {
@@ -309,33 +387,33 @@ export class Editor {
                   if (tagName === 'h1' || tagName === 'h2') {
                     italicNode.style.display = 'inline'
                   }
-                  const selectedPart = textNode.data.slice(0, endOffset)
-                  const notSelectedPart = textNode.data.slice(endOffset, textNode.length)
+                  const selectedText = textNode.data.slice(0, endOffset)
+                  const notSelectedText = textNode.data.slice(endOffset, textNode.length)
 
-                  italicNode.append(selectedPart)
-                  textNode.replaceWith(italicNode, notSelectedPart)
-                  updatedSelectedNodes.push(italicNode)
+                  italicNode.append(selectedText)
+                  textNode.replaceWith(italicNode, notSelectedText)
+                  updatedSelectedNodes.push(italicNode.firstChild)
                 }
               }
 
             }
           } else {
             console.log("text node is already wrapped")
-            updatedSelectedNodes.push(selectedNode)
+            const textNodes = this.getTextNodes(selectedNode)
+            updatedSelectedNodes.push(...textNodes)
           }
         })
       })
     }
     
     if (updatedSelectedNodes.length && !collapsed) {
-      // this.setIsItalicIconActive(!this.isItalicActive)
       const startNode = updatedSelectedNodes[0]
       const endNode = updatedSelectedNodes[updatedSelectedNodes.length - 1]
 
       Editor.selectRange({
         startNode,
-        endNode,
-        endOffset: endNode.length
+        endNode: endNode,
+        endOffset: endNode.textContent.length
       })
     }
   }
