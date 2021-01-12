03194a8b3d61e47512c2941271913d4ea6952ef9
Roman Dukhno <4opper1@gmail.com>
2021-01-12 21:48:15 +0200

    work with text nodes directly instead of working with lines

--- js/Editor.js
+++ js/Editor.js
@@ -81,10 +81,16 @@ export class Editor {
 
       return true
     })
+    // TODO make it in single iteration
+    const selectedTextNodes = []
+
+    filteredSelectedNodes.forEach(selectedNode => {
+      selectedTextNodes.push(...this.getTextNodes(selectedNode))
+    })
 
     console.log("original selectedNodes: ", selectedNodes)
 
-    return { selectedNodes: filteredSelectedNodes, filteredSelectedNodes: filteredSelectedNodes }
+    return { selectedNodes: filteredSelectedNodes, selectedTextNodes, filteredSelectedNodes: filteredSelectedNodes }
   }
 
   isDescendant = (parent, child) => {
@@ -188,10 +194,12 @@ export class Editor {
     return styleNode
   }
 
+  // TODO make divs with br on the end and beginning don't loose selection
   handleActionClick = (tagName) => {
-    const { selectedNodes, filteredSelectedNodes } = this.getSelectedNodes()
+    const { selectedTextNodes } = this.getSelectedNodes()
     const range = document.getSelection().getRangeAt(0)
-    console.log("selectedNodes: ", selectedNodes)
+    console.log("selectedTextNodes: ", selectedTextNodes)
+
     if (!range) {
       console.log("not focused or nothing is selected")
       return
@@ -202,213 +210,165 @@ export class Editor {
     let updatedStartOffset = undefined
     let updatedEndOffset = undefined
 
-    const isSelectionAlreadyWrapped = selectedNodes.every((selectedNode) => {
-      const textNodes = this.getSelectedTextNodes(selectedNode)
+    const isSelectionAlreadyWrapped = selectedTextNodes.every(textNode => {
+      const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
 
-      const isAlreadyWrapped = textNodes.every(textNode => {
-        const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-
-        return textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
-      })
-
-      return isAlreadyWrapped
+      return textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
     })
+
+    debugger
 
     if (isSelectionAlreadyWrapped) /*remove styles case*/ {
       console.log("already wrapped")
-      selectedNodes.forEach((selectedNode) => {
-        const textNodes = this.getSelectedTextNodes(selectedNode)
-        
-        if (!textNodes.length) {
-          console.log("selected node without text")
-          updatedSelectedNodes.push(selectedNode)
-        }
 
-        textNodes.forEach((textNode, index) => {
-          if (selectedNodes.length === 1) {
-            console.log("single text node")
-            const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+      selectedTextNodes.forEach((newSelectedTextNode) => {
+        if (selectedTextNodes.length === 1) {
+          console.log("single text node")
+          const textNodeParents = this.getNodeParentsUntil(newSelectedTextNode, this.editorNode)
+          const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+          const replaceWithNodes = []
+          const selectedText = range.toString()
+          const textBeforeSelected = newSelectedTextNode.data.slice(0, range.startOffset)
+          const textAfterSelected = newSelectedTextNode.data.slice(range.endOffset, newSelectedTextNode.length)
+
+          if (textBeforeSelected) {
+            const tagNode = this.createStyleNode(tagName)
+            tagNode.append(textBeforeSelected)
+            replaceWithNodes.push(tagNode)
+          }
+
+          if (selectedText) {
+            const textNodeWithoutStyle = document.createTextNode(selectedText)
+            replaceWithNodes.push(textNodeWithoutStyle)
+            updatedSelectedNodes.push(textNodeWithoutStyle)
+          }
+
+          if (textAfterSelected) {
+            const tagNode = this.createStyleNode(tagName)
+            tagNode.append(textAfterSelected)
+            replaceWithNodes.push(tagNode)
+          }
+
+          styleNode.replaceWith(...replaceWithNodes)
+        } else {
+          const isFirstNode = newSelectedTextNode === selectedTextNodes[0]
+          const isLastNode = newSelectedTextNode === selectedTextNodes[selectedTextNodes.length - 1]
+
+          if ((isFirstNode && startOffset === 0) || (isLastNode && endOffset === endContainer.length) || (!isFirstNode && !isLastNode))  {
+            console.log("one of multiple nodes is fully selected")
+            const textNodeParents = this.getNodeParentsUntil(newSelectedTextNode, this.editorNode)
             const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
-            const replaceWithNodes = []
-            const selectedText = range.toString()
-            const textBeforeSelected = textNode.data.slice(0, range.startOffset)
-            const textAfterSelected = textNode.data.slice(range.endOffset, textNode.length)
-
-            if (textBeforeSelected) {
-              const tagNode = this.createStyleNode(tagName)
-              tagNode.append(textBeforeSelected)
-              replaceWithNodes.push(tagNode)
-            }
-
-            if (selectedText) {
+            styleNode.replaceWith(styleNode.firstChild)
+            updatedSelectedNodes.push(newSelectedTextNode)
+          } else {
+            if (isFirstNode) {
+              const textNodeParents = this.getNodeParentsUntil(newSelectedTextNode, this.editorNode)
+              const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
+              const notSelectedText = newSelectedTextNode.data.slice(0, startOffset)
+              const selectedText = newSelectedTextNode.data.slice(startOffset, newSelectedTextNode.length)
               const textNodeWithoutStyle = document.createTextNode(selectedText)
-              replaceWithNodes.push(textNodeWithoutStyle)
+              const tagNode = this.createStyleNode(tagName)
+              tagNode.append(notSelectedText)
+
+              styleNode.replaceWith(tagNode, textNodeWithoutStyle)
               updatedSelectedNodes.push(textNodeWithoutStyle)
             }
 
-            if (textAfterSelected) {
-              const tagNode = this.createStyleNode(tagName)
-              tagNode.append(textAfterSelected)
-              replaceWithNodes.push(tagNode)
-            }
-
-            styleNode.replaceWith(...replaceWithNodes)
-          } else {
-            const isFirstNode = selectedNode === selectedNodes[0]
-            const isLastNode = selectedNode === selectedNodes[selectedNodes.length - 1]
-
-            if ((isFirstNode && startOffset === 0) || (isLastNode && endOffset === endContainer.length) || (!isFirstNode && !isLastNode))  {
-              console.log("one of multiple nodes is fully selected")
-              const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
+            if (isLastNode) {
+              const textNodeParents = this.getNodeParentsUntil(newSelectedTextNode, this.editorNode)
               const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
-              styleNode.replaceWith(styleNode.firstChild)
-              updatedSelectedNodes.push(textNode)
-            } else {
-              if (isFirstNode) {
-                const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-                const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
-                const notSelectedText = textNode.data.slice(0, startOffset)
-                const selectedText = textNode.data.slice(startOffset, textNode.length)
-                const textNodeWithoutStyle = document.createTextNode(selectedText)
-                const tagNode = this.createStyleNode(tagName)
-                tagNode.append(notSelectedText)
+              const selectedText = newSelectedTextNode.data.slice(0, endOffset)
+              const notSelectedText = newSelectedTextNode.data.slice(endOffset, newSelectedTextNode.length)
+              const textNodeWithoutStyle = document.createTextNode(selectedText)
+              const tagNode = this.createStyleNode(tagName)
+              tagNode.append(notSelectedText)
 
-                styleNode.replaceWith(tagNode, textNodeWithoutStyle)
-                updatedSelectedNodes.push(textNodeWithoutStyle)
-              }
-
-              if (isLastNode) {
-                const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-                const styleNode = textNodeParents.find(parentNode => parentNode.tagName.toLowerCase() === tagName)
-                const selectedText = textNode.data.slice(0, endOffset)
-                const notSelectedText = textNode.data.slice(endOffset, textNode.length)
-                const textNodeWithoutStyle = document.createTextNode(selectedText)
-                const tagNode = this.createStyleNode(tagName)
-                tagNode.append(notSelectedText)
-
-                styleNode.replaceWith(textNodeWithoutStyle, tagNode)
-                updatedSelectedNodes.push(textNodeWithoutStyle)
-              }
+              styleNode.replaceWith(textNodeWithoutStyle, tagNode)
+              updatedSelectedNodes.push(textNodeWithoutStyle)
             }
-
-
           }
-        })
+        }
       })
     } else /*add styles case*/ {
-      // TODO recheck if we use selectedNodes inside textNodes loop. Probably should use textNodes
-      selectedNodes.forEach((selectedNode, selectedNodeIndex) => {
-        const textNodes = this.getSelectedTextNodes(selectedNode)
-        const isAlreadyWrapped = textNodes.every(textNode => {
-          const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-
-          return textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
-        })
+      selectedTextNodes.forEach((newSelectedTextNode, selectedNodeIndex) => {
+        const textNodeParents = this.getNodeParentsUntil(newSelectedTextNode, this.editorNode)
+        const isAlreadyWrapped = textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
 
         if (isAlreadyWrapped) {
           console.log("whole node is already wrapped")
-          const textNodes = this.getTextNodes(selectedNode)
-          debugger
+          // const textNodes = this.getTextNodes(selectedNode)
 
           if (selectedNodeIndex === 0) {
             updatedStartOffset = endOffset
           }
 
-          if (selectedNodeIndex === selectedNodes.length - 1) {
+          if (selectedNodeIndex === selectedTextNodes.length - 1) {
             updatedEndOffset = endOffset
           }
 
-          if (textNodes.length) {
-            updatedSelectedNodes.push(...textNodes)
-          } else {
-            updatedSelectedNodes.push(selectedNode)
-          }
+          updatedSelectedNodes.push(newSelectedTextNode)
+
+          // if (textNodes.length) {
+          //   updatedSelectedNodes.push(...textNodes)
+          // } else {
+          //   updatedSelectedNodes.push(selectedNode)
+          // }
 
           return
         }
 
-        textNodes.forEach((textNode, textNodeIndex) => {
-          const textNodeParents = this.getNodeParentsUntil(textNode, this.editorNode)
-          const isTextNodeAlreadyWrapped = textNodeParents.some(parentNode => parentNode.tagName.toLowerCase() === tagName)
+        if (selectedTextNodes.length === 1) /*Single selectedNode selected or has caret*/ {
+          console.log("single selectedTextNode")
+          const replaceWithNodes = []
+          const selectedText = range.toString()
+          const textBeforeSelected = newSelectedTextNode.data.slice(0, range.startOffset)
+          const textAfterSelected = newSelectedTextNode.data.slice(range.endOffset, newSelectedTextNode.length)
+          const italicNode = this.createStyleNode(tagName)
 
-          if (!isTextNodeAlreadyWrapped) {
-            if (selectedNodes.length === 1 && textNodes.length === 1) /*Single selectedNode selected or has caret*/ {
-              console.log("single selectedNode")
-              if (!collapsed) /*Any part of selectedNode is selected*/  {
-                console.log("inner part of word or whole word")
-                const replaceWithNodes = []
-                const selectedText = range.toString()
-                const textBeforeSelected = textNode.data.slice(0, range.startOffset)
-                const textAfterSelected = textNode.data.slice(range.endOffset, textNode.length)
-                const italicNode = this.createStyleNode(tagName)
+          if (textBeforeSelected) replaceWithNodes.push(textBeforeSelected)
+          if (selectedText) replaceWithNodes.push(italicNode)
+          if (textAfterSelected) replaceWithNodes.push(textAfterSelected)
 
-                if (textBeforeSelected) replaceWithNodes.push(textBeforeSelected)
-                if (selectedText) replaceWithNodes.push(italicNode)
-                if (textAfterSelected) replaceWithNodes.push(textAfterSelected)
+          italicNode.append(selectedText)
+          newSelectedTextNode.replaceWith(...replaceWithNodes)
+          updatedSelectedNodes.push(italicNode.firstChild)
+        } else /*Multiple selectedNode selected or has caret*/ {
+          const isFirstNode = newSelectedTextNode === selectedTextNodes[0]
+          const isLastNode = newSelectedTextNode === selectedTextNodes[selectedTextNodes.length - 1]
 
-                italicNode.append(selectedText)
-                textNode.replaceWith(...replaceWithNodes)
-                updatedSelectedNodes.push(italicNode.firstChild)
-              }
-            } else /*Multiple selectedNode selected or has caret*/ {
-              const isFirstNode = selectedNode === selectedNodes[0]
-              const isLastNode = selectedNode === selectedNodes[selectedNodes.length - 1]
+          if ((isFirstNode && startOffset === 0) || (isLastNode && endOffset === endContainer.length) || (!isFirstNode && !isLastNode))  {
+            console.log("one of multiple nodes is fully selected")
+            const italicNode = this.createStyleNode(tagName)
+            const clonedNode = newSelectedTextNode.cloneNode()
 
-              if ((isFirstNode && startOffset === 0) || (isLastNode && endOffset === endContainer.length) || (!isFirstNode && !isLastNode))  {
-                console.log("one of multiple nodes is fully selected")
-                const italicNode = this.createStyleNode(tagName)
-                const clonedNode = textNode.cloneNode()
-
-                italicNode.append(clonedNode)
-                textNode.replaceWith(italicNode)
-                updatedSelectedNodes.push(italicNode.firstChild)
-              } else {
-                if (isFirstNode) {
-                  console.log("first of multiple nodes is not fully selected")
-                  const italicNode = this.createStyleNode(tagName)
-                  const notSelectedText = textNode.data.slice(0, startOffset)
-                  const selectedText = textNode.data.slice(startOffset, textNode.length)
-
-                  italicNode.append(selectedText)
-                  textNode.replaceWith(notSelectedText, italicNode)
-                  updatedSelectedNodes.push(italicNode.firstChild)
-                }
-
-                // TODO FIx 3 lines, second line select middle chars at least 2, select part of first line and part of chars from the second line
-                // Think maybe selectedNodes should containt selectedText nodes instead of parents. This will allow to get rid of multiple
-                // iterations and problems with deciding what to use selectedNode or textNode
-                if (isLastNode) {
-                  console.log("last of multiple nodes is not fully selected")
-                  const italicNode = this.createStyleNode(tagName)
-                  const selectedText = textNode.data.slice(0, endOffset)
-                  const notSelectedText = textNode.data.slice(endOffset, textNode.length)
-
-                  italicNode.append(selectedText)
-                  textNode.replaceWith(italicNode, notSelectedText)
-                  updatedSelectedNodes.push(italicNode.firstChild)
-                }
-              }
-
-            }
+            italicNode.append(clonedNode)
+            newSelectedTextNode.replaceWith(italicNode)
+            updatedSelectedNodes.push(italicNode.firstChild)
           } else {
-            console.log("text node is already wrapped")
-            const textNodes = this.getTextNodes(selectedNode)
+            if (isFirstNode) {
+              console.log("first of multiple nodes is not fully selected")
+              const italicNode = this.createStyleNode(tagName)
+              const notSelectedText = newSelectedTextNode.data.slice(0, startOffset)
+              const selectedText = newSelectedTextNode.data.slice(startOffset, newSelectedTextNode.length)
 
-            if (selectedNodeIndex === 0) {
-              updatedStartOffset = endOffset
+              italicNode.append(selectedText)
+              newSelectedTextNode.replaceWith(notSelectedText, italicNode)
+              updatedSelectedNodes.push(italicNode.firstChild)
             }
 
-            if (selectedNodeIndex === selectedNodes.length - 1) {
-              updatedEndOffset = endOffset
-            }
+            if (isLastNode) {
+              console.log("last of multiple nodes is not fully selected")
+              const italicNode = this.createStyleNode(tagName)
+              const selectedText = newSelectedTextNode.data.slice(0, endOffset)
+              const notSelectedText = newSelectedTextNode.data.slice(endOffset, newSelectedTextNode.length)
 
-            if (textNodes.length) {
-              updatedSelectedNodes.push(...textNodes)
-            } else {
-              updatedSelectedNodes.push(selectedNode)
+              italicNode.append(selectedText)
+              newSelectedTextNode.replaceWith(italicNode, notSelectedText)
+              updatedSelectedNodes.push(italicNode.firstChild)
             }
           }
-        })
+
+        }
       })
     }
     
